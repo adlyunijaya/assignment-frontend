4ff668ec59f4556e76e1e27c7270524069586589
PandaGemuk <adlyalimin@gmail.com>
2020-10-19 22:50:45 +0800

    Initial Commit

---
+++ resources/views/extras/top_bar.blade.php
@@ -0,0 +1,3 @@
+<!-- BEGIN: Breadcrumb -->
+<div class="-intro-x breadcrumb mr-auto hidden sm:flex"> <a href="#" class="">Dashboard</a> <i data-feather="chevron-right" class="breadcrumb__icon"></i> <a href="#" class="breadcrumb--active">Posts</a> </div>
+<!-- END: Breadcrumb -->
